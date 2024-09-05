cf2e851d9a1aa6ddfdea31c3a31a00513d47269e
Rafhael Banuls <100212199+banola@users.noreply.github.com>
2024-09-05 20:53:38 -0300

    Add files via upload

---
+++ crud-java-spring-main/src/main/resources/db/migration/V1__create-table-product.sql
@@ -0,0 +1,6 @@
+CREATE TABLE product (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    price INT NOT NULL,
+    description TEXT NOT NULL
+);
---
+++ crud-java-spring-main/src/main/resources/db/migration/V2__alter-table-product.sql
@@ -0,0 +1,3 @@
+ALTER TABLE product ADD active BOOLEAN;
+UPDATE product SET active = true;
+/*ALTER TABLE product ADD COLUMN description TEXT NOT NULL;*/
